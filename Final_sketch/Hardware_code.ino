b6b3d7d631196d0fe30bd351cd812fd401f5d50f
sahil1239 <76108166+sahil1239@users.noreply.github.com>
2023-11-26 16:49:10 +0530

    Updated Hardware_code.ino

--- Final_sketch/Hardware_code.ino
+++ Final_sketch/Hardware_code.ino
@@ -17,15 +17,17 @@ float roll, pitch, yaw;
 const char* ssid = "Sahil's S23";
 const char* password = "sahil2807";
 
-const char* serverAddress = "192.168.83.218"; // or hostname
+const char* serverAddress = "192.168.7.219"; // or hostname
 const int serverPort = 8080;
 
 float latitude , longitude;
-int year , month , date, hour , minute , second;
-String date_str;
-String time_str;
+// int year , month , date, hour , minute , second;
+// String date_str;
+// String time_str;
 String lat_str;
 String lng_str;
+String model = "Maruti swift";
+String number = "CH01AA1234";
 int pm;
 
 WiFiClient client;
@@ -89,61 +91,61 @@ void loop() {
         longitude = gps.location.lng();
         lng_str = String(longitude , 6); //longitude location is stored in a string
       }
-      if (gps.date.isValid()) //check whether gps date is valid
-      {
-        date_str = "";
-        date = gps.date.day();
-        month = gps.date.month();
-        year = gps.date.year();
-        if (date < 10)
-          date_str = '0';
-        date_str += String(date);// values of date,month and year are stored in a string
-        date_str += " / ";
+      // if (gps.date.isValid()) //check whether gps date is valid
+      // {
+      //   date_str = "";
+      //   date = gps.date.day();
+      //   month = gps.date.month();
+      //   year = gps.date.year();
+      //   if (date < 10)
+      //     date_str = '0';
+      //   date_str += String(date);// values of date,month and year are stored in a string
+      //   date_str += " / ";
 
-        if (month < 10)
-          date_str += '0';
-        date_str += String(month); // values of date,month and year are stored in a string
-        date_str += " / ";
-        if (year < 10)
-          date_str += '0';
-        date_str += String(year); // values of date,month and year are stored in a string
-      }
-      if (gps.time.isValid())  //check whether gps time is valid
-      {
-        time_str = "";
-        hour = gps.time.hour();
-        minute = gps.time.minute();
-        second = gps.time.second();
-        minute = (minute + 30); // converting to IST
-        if (minute > 59)
-        {
-          minute = minute - 60;
-          hour = hour + 1;
-        }
-        hour = (hour + 5) ;
-        if (hour > 23)
-          hour = hour - 24;   // converting to IST
-        if (hour >= 12)  // checking whether AM or PM
-          pm = 1;
-        else
-          pm = 0;
-        hour = hour % 12;
-        if (hour < 10)
-          time_str = '0';
-        time_str += String(hour); //values of hour,minute and time are stored in a string
-        time_str += " : ";
-        if (minute < 10)
-          time_str += '0';
-        time_str += String(minute); //values of hour,minute and time are stored in a string
-        time_str += " : ";
-        if (second < 10)
-          time_str += '0';
-        time_str += String(second); //values of hour,minute and time are stored in a string
-        if (pm == 1)
-          time_str += " PM ";
-        else
-          time_str += " AM ";
-      }
+      //   if (month < 10)
+      //     date_str += '0';
+      //   date_str += String(month); // values of date,month and year are stored in a string
+      //   date_str += " / ";
+      //   if (year < 10)
+      //     date_str += '0';
+      //   date_str += String(year); // values of date,month and year are stored in a string
+      // }
+      // if (gps.time.isValid())  //check whether gps time is valid
+      // {
+      //   time_str = "";
+      //   hour = gps.time.hour();
+      //   minute = gps.time.minute();
+      //   second = gps.time.second();
+      //   minute = (minute + 30); // converting to IST
+      //   if (minute > 59)
+      //   {
+      //     minute = minute - 60;
+      //     hour = hour + 1;
+      //   }
+      //   hour = (hour + 5) ;
+      //   if (hour > 23)
+      //     hour = hour - 24;   // converting to IST
+      //   if (hour >= 12)  // checking whether AM or PM
+      //     pm = 1;
+      //   else
+      //     pm = 0;
+      //   hour = hour % 12;
+      //   if (hour < 10)
+      //     time_str = '0';
+      //   time_str += String(hour); //values of hour,minute and time are stored in a string
+      //   time_str += " : ";
+      //   if (minute < 10)
+      //     time_str += '0';
+      //   time_str += String(minute); //values of hour,minute and time are stored in a string
+      //   time_str += " : ";
+      //   if (second < 10)
+      //     time_str += '0';
+      //   time_str += String(second); //values of hour,minute and time are stored in a string
+      //   if (pm == 1)
+      //     time_str += " PM ";
+      //   else
+      //     time_str += " AM ";
+      // }
     }
   }
     Wire.beginTransmission(MPU);
@@ -184,9 +186,9 @@ void loop() {
 
     // Send the orientation angles to the connected client
     String data1 = String(AccX)+","+String(AccY)+","+String(AccZ)+","+String(GyroX)+","+String(GyroY)+","+String(GyroZ)+","+String(roll)+","+String(pitch)+","+String(yaw);
-    String data2 = lat_str+","+lng_str+","+time_str+","+date_str;
+    String data2 = lat_str+","+lng_str+","+model+","+number;
     String to_send = data1+","+data2;
     client.println(to_send);
   }
-  delay(1000);
+  delay(10);
 }
